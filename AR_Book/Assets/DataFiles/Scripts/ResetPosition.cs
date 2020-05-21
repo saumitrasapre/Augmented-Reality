e0404f168f605c93f7ba6c9a3e6f7d76c0d16877
Saumitra Sapre <saumitrasapre69@gmail.com>
2020-05-22 04:55:24 +0530

    Tested AR on hand drawn image objects to make an AR book

---
+++ AR_Book/Assets/DataFiles/Scripts/ResetPosition.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+    Vector3 originalPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalPos = transform.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collide)
+    {
+        if(collide.gameObject.CompareTag("ShipWall"))
+        {
+            transform.localPosition = originalPos;
+        }
+        else if(collide.gameObject.CompareTag("AirplaneWall"))
+        {
+            transform.localPosition = originalPos;
+        }
+        else if(collide.gameObject.CompareTag("CloudWall"))
+        {
+            transform.localPosition = originalPos;
+        }
+    }
+}
