798e93a06623d52187021b8e3f79ae2433c6bb8a
Saumitra Sapre <saumitrasapre69@gmail.com>
2020-05-23 20:13:54 +0530

    Tested virtual buttons in Vuforia

---
+++ Buttons/Assets/DataFiles/Scripts/VirtualButtonScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject sphere, cube;
+    VirtualButtonBehaviour vrb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vrb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrb.RegisterEventHandler(this);
+        cube.SetActive(true);
+        sphere.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cube.SetActive(false);
+        sphere.SetActive(true);
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cube.SetActive(true);
+        sphere.SetActive(false);
+    }
+}
